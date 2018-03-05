c7eb79004bcd34634225593f04d1f715219a5093
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-05 23:02:18 +0100

    Add middleware /store

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
+app.use('/store', function(req, res, next) {
 	console.log('Jestem pośrednikiem przy żądaniu do /store');
 	next();
 });
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
+});
+
+app.get('/store', function(req, res) {
+	res.send('To jest sklep');
 });
 
 app.listen(3000);
